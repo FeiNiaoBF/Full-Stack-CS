e1a844642bdbb6c0143eda35356642e43682e673
Yeelght <91862060+FeiNiaoBF@users.noreply.github.com>
2023-12-10 21:42:47 +0800

    Done:part1-ex2 --3h

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,10 +1,9 @@
 
 /// Header
 const Header = (props) => {
+    console.log(props)
     return (
-        <div>
-            <h1>{props.course}</h1>
-        </div>
+        <h1>{props.course}</h1>
     )
 }
 
@@ -12,9 +11,9 @@ const Header = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part part={props.part[0].part1} exercises={props.part[0].exercises1} />
-            <Part part={props.part[1].part2} exercises={props.part[1].exercises2} />
-            <Part part={props.part[2].part3} exercises={props.part[2].exercises3} />
+            <Part name={props.part[0].name} exercises={props.part[0].exercises} />
+            <Part name={props.part[1].name} exercises={props.part[1].exercises} />
+            <Part name={props.part[2].name} exercises={props.part[2].exercises} />
         </div>
     );
 };
@@ -22,36 +21,50 @@ const Content = (props) => {
 /// Part
 const Part = (props) => {
     return (
-        <div>
-            <p>{props.part} {props.exercises}</p>
-        </div>
+        <p>{props.name} {props.exercises}</p>
     )
 }
 
 /// Total
 const Total = (props) => {
+    let total = 0;
+    for (let i = 0; i < props.exercises.length; i++) {
+        total += props.exercises[i].exercises;
+    }
     return (
         <div>
-            <p>Number of exercises {props.exercises}</p>
+            <p>Number of exercises {total}</p>
         </div>
     )
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-    // 重构Content组件，使其本身不渲染任何部件的名称或其练习次数。
-    // 相反，它只渲染三个Part组件，每个组件渲染一个部分的名称和练习的次数。
+    // Object of parts
+    const course = {
+        name: 'Half Stack application development',
+        // array of part
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+
     return (
         <div>
-            <Header course={course} />
-            <Content part={[{ part1, exercises1 }, { part2, exercises2 }, { part3, exercises3 }]} />
-            <Total exercises={exercises1 + exercises2 + exercises3} />
+            <Header course={course.name} />
+            <Content part={course.parts} />
+            <Total exercises={course.parts} />
         </div>
     )
 }
