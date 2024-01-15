e8059052d06849eb0cf6596a06160faf1e8fd3eb
yeelight <91862060+FeiNiaoBF@users.noreply.github.com>
2024-01-15 22:35:13 +0800

    Function return the function

--- part1/src/App.js
+++ part1/src/App.js
@@ -31,31 +31,50 @@ const History = (props) => {
   return <div>Button press history: {props.allClicks.join(' ')}</div>
 }
 
+// const App = () => {
+//   const [clicks, setClicks] = useState({
+//     left: 0,
+//     right: 0
+//   })
+//   const [allClicks, setAll] = useState([])
+//   debugger
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setClicks({ ...clicks, left: clicks.left + 1 })
+//   }
+
+//   const handleRightClick = () => {
+//     setClicks({ ...clicks, right: clicks.right + 1 })
+//     setAll(allClicks.concat('R'))
+//   }
+
+//   return (
+//     <div>
+//       {clicks.left}
+//       <Button onClick={handleLeftClick} text="left" />
+//       <Button onClick={handleRightClick} text="right" />
+//       {clicks.right}
+//       <History allClicks={allClicks} />
+//     </div>
+//   )
+// }
+
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0
-  })
-  const [allClicks, setAll] = useState([])
-  debugger
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setClicks({ ...clicks, left: clicks.left + 1 })
-  }
+    const [value, setValue] = useState(10)
 
-  const handleRightClick = () => {
-    setClicks({ ...clicks, right: clicks.right + 1 })
-    setAll(allClicks.concat('R'))
-  }
+    const setToVal = (nv) => () => {
+        console.log('value is', nv)
+        setValue(nv)
+    }
 
-  return (
-    <div>
-      {clicks.left}
-      <Button onClick={handleLeftClick} text="left" />
-      <Button onClick={handleRightClick} text="right" />
-      {clicks.right}
-      <History allClicks={allClicks} />
-    </div>
-  )
+    return (
+      <div>
+        <h1>Greetings</h1>
+        {value}
+        <button onClick={setToVal(0)}>reset to zero</button>
+        <button onClick={setToVal(1000)}>reset to thousand</button>
+        <button onClick={setToVal(value + 1)}>increment</button>
+      </div>
+    )
 }
 export default App
