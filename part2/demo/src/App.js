ae4970de8841813745a8eb818031e375efc1baaa
yeelight <91862060+FeiNiaoBF@users.noreply.github.com>
2024-03-16 22:08:13 +0800

    Filtering Displayed Elements

--- part2/demo/src/App.js
+++ part2/demo/src/App.js
@@ -4,6 +4,7 @@ import Note from './components/Note';
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNode, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
 
   const addNode = (event) => {
     event.preventDefault();
@@ -23,12 +24,21 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
   // console.log(notes);
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show to {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
